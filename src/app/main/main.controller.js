e85a69cee80aed08247230a4b1f8e331d2743681
Marco  Livraghi <m.livraghi@digitelematica.com>
2016-05-07 19:41:45 +0200

    add markers on click

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,10 +8,42 @@
   /** @ngInject */
   function MainController($scope) {
     var vm = this;
-
+	vm.markersCount=0;
+	vm.events= {
+            map: {
+                enable: ['drag', 'click'],
+                logic: 'emit'
+            }
+        };
+	vm.markers={
+		
+	};
+		
+$scope.$on('leafletDirectiveMap.click', function(event, args){
+	vm.addMarker(args.leafletEvent.latlng.lat,args.leafletEvent.latlng.lng);
+});
+   
+	
+	
     $scope.defaults= {
             scrollWheelZoom: false
         }
-		
+	
+	function addMarker(latitude,longitude)
+	{
+		vm.markersCount++;
+		vm.markers["m"+vm.markersCount]={
+			lat:latitude,
+			lng:longitude,
+			focus: true,
+			message: 'Marker #'+vm.markersCount,
+			draggable: false
+		};
+	}
+	
+	
+	vm.addMarker=addMarker;
+	
+	
     }
 })();
