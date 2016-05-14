50c4e8d4fae04b35da0e20d59bfc9d479a1568ff
Marco  Livraghi <m.livraghi@digitelematica.com>
2016-05-14 11:19:11 +0200

    show markers

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,6 +8,9 @@
   /** @ngInject */
   function MainController($scope,MarkerService,$log) {
     var vm = this;
+	
+	vm.markerPairList={};
+	
 	vm.markers1Count=0;
 	vm.markers2Count=0;
 	vm.events1= {
@@ -39,25 +42,56 @@ $scope.$on('leafletDirectiveMap.map2.click', function(event, args){
 	function addMarker1(latitude,longitude)
 	{
 		vm.markers1Count++;
-		vm.markers1["m"+vm.markers1Count]={
+		
+		var newMarker={
 			lat:latitude,
 			lng:longitude,
 			focus: true,
 			message: 'Marker #'+vm.markers1Count,
 			draggable: false
 		};
+		
+		if (!vm.markerPairList["m"+vm.markers1Count])
+		{
+			vm.markerPairList["m"+vm.markers1Count]={};
+			vm.markerPairList["m"+vm.markers1Count].markerPairId=vm.markers1Count;
+		}
+		
+		vm.markerPairList["m"+vm.markers1Count].marker1=newMarker;
+		
+		
+		vm.markers1["m"+vm.markers1Count]=newMarker;
 	}
 	
 	function addMarker2(latitude,longitude)
 	{
 		vm.markers2Count++;
-		vm.markers2["m"+vm.markers2Count]={
+		
+		var newMarker={
 			lat:latitude,
 			lng:longitude,
 			focus: true,
 			message: 'Marker #'+vm.markers2Count,
 			draggable: false
 		};
+		
+		
+		if (!vm.markerPairList["m"+vm.markers2Count])
+		{
+			vm.markerPairList["m"+vm.markers2Count]={};
+			vm.markerPairList["m"+vm.markers2Count].markerPairId=vm.markers2Count;
+		}
+		
+		vm.markerPairList["m"+vm.markers2Count].marker2=newMarker;
+		
+		vm.markers2["m"+vm.markers2Count]=newMarker;
+	}
+	
+	function highLightMarker(index)
+	{
+		vm.markers1["m"+index].focus=true;
+		vm.markers2["m"+index].focus=true;
+		$log.debug(vm.markers1);
 	}
 	
 	function sendMarker()
@@ -75,6 +109,7 @@ $scope.$on('leafletDirectiveMap.map2.click', function(event, args){
 	
 	vm.addMarker1=addMarker1;
 	vm.addMarker2=addMarker2;
+	vm.highLightMarker=highLightMarker;
 	vm.sendMarker=sendMarker;
 	
 	
