5ddd442615875854dfdcc2c40d01fe3c0750c23b
Marco  Livraghi <m.livraghi@digitelematica.com>
2016-05-16 21:46:40 +0200

    library and soruce files ok

---
+++ src/main/java/it/polimi/gis/core/Pair.java
@@ -0,0 +1,25 @@
+package it.polimi.gis.core;
+
+import com.vividsolutions.jts.geom.Point;
+
+public class Pair {
+    private Point pointA;
+    private Point pointB;
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+    
+}
