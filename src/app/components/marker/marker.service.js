e2f0adde352e1c080de5e6bdfa9364485aeb8705
Marco  Livraghi <m.livraghi@digitelematica.com>
2016-06-13 20:31:33 +0200

    save points from layer

--- src/app/components/marker/marker.service.js
+++ src/app/components/marker/marker.service.js
@@ -10,7 +10,7 @@ function MarkerService($http)
 
 this.send = function(markerObject) {
 var markerArray = Object.keys(markerObject).map(function (key) {return markerObject[key]});
-var promise= $http.post("http://127.0.0.1:8080/GisProjectServer/map?layer1=ciao&layer2=miao",markerArray);
+var promise= $http.post("http://127.0.0.1:8080/GisProjectServer/map?layer1=OSM_00_4326&layer2=DBT_00_4326",markerArray);
 return promise; 
 };
 
